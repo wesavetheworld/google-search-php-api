59ee702fd2ae0ac41bd80c4c106cb754ceb5b5e2
Eugen Saraci <eugen.saraci@gmail.com>
2016-06-26 18:25:43 +0200

    added assets.php

--- search.php
+++ search.php
@@ -6,19 +6,16 @@
 <body>
 
 <?php
+require('assets.php');
 if(isset($_GET['input']) && $_GET['input'] != "")
 {   
 
     //GET vars
     $query = urlencode($_GET['input']);
-    $cx = $_GET['sekey']; //"001186922872885026417:9ptvru9qau8";
-    $key = $_GET['gapikey']; //"AIzaSyB8iJub21LJvWSQcMNLLzkyS92XxD9lheQ"; 
-    $mhapikey = $_GET['mhapikey']; //4b7444747fde2d24acaa34daf62cf1d1c53161fa:
     $numero=$_GET['numero'];
-    $howmanyemailaddresses = 5;
     
-    $decine=floor($numero/10);
-    $unita=$numero%10;
+    $dec=floor($numero/10);
+    $uni=$numero%10;
 
     echo '<h1> Results </h1>
             <strong>Keyword: </strong> '.$_GET["input"].' <br />
@@ -27,8 +24,7 @@ if(isset($_GET['input']) && $_GET['input'] != "")
 
 
     function stampa_risultati($q, $start, $limite) {
-        
-        $url =  "https://www.googleapis.com/customsearch/v1?q=$q&cx=".$_GET['sekey']."&key=".$_GET['gapikey']."&start=$start";
+        $url =  "https://www.googleapis.com/customsearch/v1?q=$q&cx=".$GLOBALS['cx']."&key=".$GLOBALS['gapikey']."&start=$start";
         $json = file_get_contents($url);
         $data = json_decode($json, TRUE);
         $z=0;
@@ -39,35 +35,33 @@ if(isset($_GET['input']) && $_GET['input'] != "")
                 if ($z != $limite) {
                     print '<center>';
                     print  '<h2>'.$item["displayLink"].'</h2>';
-                    echo '
-                            <table width="50%" border="1">
-                            <tr>
-                                <th>Address</th>
-                                <th>Type</th>
-                                <th>Status</th>
-                                <th>Score</th>
-                                <th>Send?</th>
-                            </tr>';
+                    if (($_GET['emails']=='emails')) {
+                        echo '
+                                <table width="50%" border="1">
+                                <tr>
+                                    <th>Address</th>
+                                    <th>Type</th>
+                                    <th>Status</th>
+                                    <th>Score</th>
+                                    <th>Send?</th>
+                                </tr>';
 
-                    $json2 = file_get_contents("https://api.emailhunter.co/v1/search?domain=".preg_replace('#^www\.(.+\.)#i', '$1', $item['displayLink'])."&api_key=".$_GET['mhapikey']);
-                    $array2 = json_decode($json2, TRUE);
-                    foreach($array2['emails'] as $email) {
-                        if ($m!=5) {
-                            print '<tr><td align="center" id="'.$email["value"].'">'.$email["value"].'</td>';
-                            print  '<td align="center">'.$email["type"].'</td>';
-                            $json3 = file_get_contents("https://api.emailhunter.co/v1/verify?email=".$email['value']."&api_key=".$_GET['mhapikey']);
-                            $array3 = json_decode($json3, TRUE);
-                            print  '<td align="center">' .$array3["result"]. '</td>';
-                            print '<td align="center">'.$array3["score"].'</td>';
-                            print '<td align="center"><input type="checkbox" id="CHK'.$email["value"].'" onchange="activate(this.id)"></td>';
-                            $m++;
-                        }
+                        $json2 = file_get_contents("https://api.emailhunter.co/v1/search?domain=".preg_replace('#^www\.(.+\.)#i', '$1', $item['displayLink'])."&api_key=".$GLOBALS['mhapikey']);
+                        $array2 = json_decode($json2, TRUE);
+                        foreach($array2['emails'] as $email) {
+                            if ($m!=$GLOBALS['nremail']) {
+                                print '<tr><td align="center" id="'.$email["value"].'">'.$email["value"].'</td>';
+                                print  '<td align="center">'.$email["type"].'</td>';
+                                $json3 = file_get_contents("https://api.emailhunter.co/v1/verify?email=".$email['value']."&api_key=".$GLOBALS['mhapikey']);
+                                $array3 = json_decode($json3, TRUE);
+                                print  '<td align="center">' .$array3["result"]. '</td>';
+                                print '<td align="center">'.$array3["score"].'</td>';
+                                print '<td align="center"><input type="checkbox" id="CHK'.$email["value"].'" onchange="activate(this)"></td>';
+                                $m++;
+                            }
+                        }       
+                     print ' </table></center>';
                     }
-
-                            
-
-
-                    print ' </table></center>';
                     print '</div><br /><br />';
                     $z++;
                 }  
@@ -75,7 +69,7 @@ if(isset($_GET['input']) && $_GET['input'] != "")
         }
     }
 
-   for ($x=0; $x < $decine; $x++) {
+   for ($x=0; $x < $dec; $x++) {
         if ($x>0) {
             stampa_risultati($query, 10*$x, 10);
         } else {
@@ -83,16 +77,16 @@ if(isset($_GET['input']) && $_GET['input'] != "")
         }
     }
 
-    if ($unita!=0) {
-        if ($decine!=0)
-            stampa_risultati($query, 10*$decine, $unita);
+    if ($uni!=0) {
+        if ($dec!=0)
+            stampa_risultati($query, 10*$dec, $uni);
         else 
-            stampa_risultati($query, 1, $unita);
+            stampa_risultati($query, 1, $uni);
     }
 }
 ?>
 
-<button onclick="raccogli()">Raccogli</button>
+<input type="button" onclick="raccogli()" value="Raccogli" />
 <p id="listo">Gente a cui spedire:</p>
 
 <script>
@@ -101,7 +95,7 @@ function raccogli() {
 	
 	var i;
 	for (i=0; i<list.length; i++) {
-		document.getElementById("listo").innerHTML += list[i].id.toString();
+		document.getElementById("listo").innerHTML += list[i].innerHTML;
 	}
 }
 
