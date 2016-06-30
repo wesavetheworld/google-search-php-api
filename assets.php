460fa063733b69e6069aa4a5f4162d7499f8b347
Eugen Saraci <eugen.saraci@gmail.com>
2016-06-30 17:09:31 +0200

    removed sendgrid variable

--- assets.php
+++ assets.php
@@ -4,6 +4,5 @@
     $gapikey = "AIzaSyB8iJub21LJvWSQcMNLLzkyS92XxD9lheQ"; //api key, free = 100 queries per day, 5$ = +1000 queries
     $mhapikey = "0db56b0f37ab16f198de6db218869940d56d26f7";	//emailhunter api key, free = 150 queries per month, check site for premium plans.
     $nremail = 3; //number of emails to retrieve for each site.
-    $sgapikey = 0; //wont be needed if the php server supports the mail() function.
 
 ?>
