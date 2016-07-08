aee91465b0557327ef906a2c4c78422b4c99e7f9
esaraci <eugen.saraci@gmail.com>
2016-07-08 13:24:35 +0200

    Update assets.php

--- assets.php
+++ assets.php
@@ -1,8 +1,8 @@
 <?php
     
-    $cx = "001186922872885026417:9ptvru9qau8"; //search engine id
-    $gapikey = "AIzaSyB8iJub21LJvWSQcMNLLzkyS92XxD9lheQ"; //api key, free = 100 queries per day, 5$ = +1000 queries
-    $mhapikey = "0db56b0f37ab16f198de6db218869940d56d26f7";	//emailhunter api key, free = 150 queries per month, check site for premium plans.
+    $cx = ""; //search engine id
+    $gapikey = ""; //api key, free = 100 queries per day, 5$ = +1000 queries
+    $mhapikey = """;	//emailhunter api key, free = 150 queries per month, check site for premium plans.
     $nremail = 3; //number of emails to retrieve for each site.
 
 ?>
